14fa894a0329f7c5712f91bc8740e784998b74e4
elenamlopez <elena.mateos1@gmail.com>
2018-05-14 22:57:03 +0200

    Added dices

---
+++ dice/add_member.js
@@ -0,0 +1,9 @@
+var addButton = document.getElementById('addPlayerBtn'),
+    playersList = document.getElementById('playersList');
+
+addButton.addEventListener('click', function(){
+  var addPlayer = document.getElementById('playerToAdd').value;
+
+  console.log(addPlayer);
+  playersList.innerHTML+= '<li class="player-element">'+ addPlayer +'<button class="custom-button player-button" type="button" name="button">Tirar!</button> </li>';
+})
---
+++ dice/dice.js
@@ -0,0 +1,25 @@
+/**
+  * Tirada de dados:
+  *
+*/
+var startBtn = document.getElementById('start');
+
+startBtn.addEventListener('click', function(){
+
+  var rollButton = document.getElementsByClassName('player-button'),
+      dice1 = document.getElementById('dice1'),
+      dice2 = document.getElementById('dice2');
+
+  function hide(element){
+    element.classList.toggle('hidden');
+  }
+  for (var i = 0; i < rollButton.length; i++) {
+    rollButton[i].addEventListener('click', function(){
+      dice1.value =  Math.floor(Math.random()*10)+1; // como patron entraría tb en una funcion
+      dice2.value =  Math.floor(Math.random()*10)+1;
+      console.log('dado1: '+ dice1.value);
+      console.log('dado2: '+ dice2.value);
+      hide(this);
+    });
+  }
+})
